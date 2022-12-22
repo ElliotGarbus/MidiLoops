10fbdfafbc03bd1486958e1dc8df24d107b2d05b
ElliotGarbus <elliotg2@cox.net>
2022-12-22 15:26:07 -0700

    implemented play next feature

--- playscreen.py
+++ playscreen.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.core.audio import SoundLoader
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
-from kivy.clock import Clock
 from kivy.uix.popup import Popup
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
@@ -232,6 +231,6 @@ class PlayScreen(Screen):
                 break
 
     def update_play_state(self):
-        state = [w.ids.tb.state for w in self.ids.scroll_box.children[::-1]]
+        state = [w.ids.tb.state for w in self.ids.scroll_box.children]
         self.is_playing = 'down' in state
 
